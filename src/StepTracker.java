3e0856b450651332c38e5a40214e1b19abb1d2c4
JoannnW <143521580+JoannnW@users.noreply.github.com>
2024-08-24 19:20:11 +0800

    idk if this works - it doesn't run

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,67 +1,47 @@
-import java.util.Scanner;
 public class StepTracker{
     //properties
-    private int stepsForActiveDay;
+    private int minSteps;
     private int activeDays;
+    private int steps;
     private int days;
     //constructor
-    public StepTracker(int stepsForActiveDay){
-        this.stepsForActiveDay=stepsForActiveDay;
-        this.days=0;
-        this.activeDays=0;
+    public StepTracker(int minSteps){
+        this.minSteps=minSteps;
+        activeDays=0;
+        steps=0;
+        days=0;
     }
     //get__ methods for each property(3)
-    public int getStepsForActiveDay(){
-        return stepsForActiveDay;
+    public int getMinSteps(){
+        return minSteps;
     }
     public int getActiveDays(){
         return activeDays;
+    }
+    public int getSteps(){
+        return steps;
     }
     public int getDays(){
         return days;
     }
 
-    public int[] DailySteps(int days){
-        int number=1;
-        int[] dailySteps=new int[days];//new array=size of no. of days.
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < days; i++) {
-            if (days<1){
-                break;
-            }
-
-            System.out.println("Day "+number+" steps: ");
-            dailySteps[number]=scanner.nextInt();
-            number++;
+    //Methods
+    public void addDailySteps(int steps){
+        if (steps>minSteps){
+            activeDays++;
         }
-        return dailySteps;
+        days++;
+        this.steps+=steps;
     }
-
-    public int activeDays(int stepsForActiveDay, int[] dailySteps){
-        for (int i=0;i<dailySteps.length-1;i++){
-            if (i>stepsForActiveDay){
-                activeDays++;
-            }
-        }
+    public int activeDays(){
         return activeDays;
     }
-
-    public int addDailySteps(int[]dailySteps){
-        int totalSteps=0;
-        for (int i=0;i<dailySteps.length-1;i++){
-            totalSteps+=dailySteps[i];
-        }
-        return totalSteps;
-    }
-
-    public double averageSteps(int totalSteps){
-        if(totalSteps==0){//nts: Use == for equality check, not =
+    public double averageSteps(){
+        if (days==0){
             return 0.0;
         }
-        return (double)totalSteps/days; //nts: (double) in front of totalSteps converts...
-        // ...the int into a double, hence makeing the result of...
-        // ...(double)totalSteps/totalDays a double
+        return (double) steps /days;
     }
-
 }
+
 
